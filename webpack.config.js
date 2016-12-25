42ea4dff6ca8bb70e1bd6d575aa48196a2df33a8
yajamon <yajamon.tatsuki@gmail.com>
2016-12-25 21:02:45 +0900

    Concat module

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+var webpack = require('webpack');
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: {
+        bundle: "./src/index.tsx",
+        vendor: ["react", "react-dom"]
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].js",
         path: __dirname + "/dist"
     },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor' // Specify the common bundle's name.
+        })
+    ],
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-maps",
@@ -23,14 +32,5 @@ module.exports = {
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { test: /\.js$/, loader: "source-map-loader" }
         ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+    }
 }
