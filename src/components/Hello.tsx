bd49924dc3d6fae55769b7d074a28545c36f727a
yajamon <yajamon.tatsuki@gmail.com>
2016-12-25 14:48:14 +0900

    Edit Hello.tsx
    fxxk document.
    arrow functionなんだから引数参照するのにthisがいるわけないでしょう。

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export interface HelloProps { compiler: string; framework: string; }
 
 // stateless functional components
-export const Hello = (props: HelloProps) => <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>
 
 // export class Hello extends React.Component<HelloProps, undefined> {
 //     render() {
