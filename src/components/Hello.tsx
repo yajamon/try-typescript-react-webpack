4dc4c1a21e2d36a0639f934a10935e2a43c60181
yajamon <yajamon.tatsuki@gmail.com>
2016-12-25 14:06:48 +0900

    small fix
    lost export keyword at document

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,9 @@
-import * as React from "react"
+import * as React from "react";
 
 export interface HelloProps { compiler: string; framework: string; }
 
 // stateless functional components
-const Hello = (props: HelloProps) => <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+export const Hello = (props: HelloProps) => <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
 
 // export class Hello extends React.Component<HelloProps, undefined> {
 //     render() {
